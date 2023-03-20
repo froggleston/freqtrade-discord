a9f11c8e86479e1b423ad8387ca114e87f0f21a0
froggleston <robertdavey@carpentries.org>
2023-03-20 11:50:57 +0000

    Tidy up code a lot, improve calling of API functions and error handling, add support for disabled commands

--- ft_bot.py
+++ ft_bot.py
@@ -24,12 +24,9 @@ import argparse
 import discord
 import logging
 import traceback
+from typing import Any, Dict, List, Optional
 
 from tabulate import tabulate
-
-token = None
-auth = None
-servers = {}
 
 logging.basicConfig(
     level=logging.INFO,
@@ -39,10 +36,107 @@ logger = logging.getLogger("ft_bot")
 
 class ft_bot(discord.Client):
 
-    def _on_ready(self):
-        print(f'We have logged in as {self.user}')
+    def __init__(self,
+                 intents: discord.Intents, 
+                 servers: Dict,
+                 disabled_calls: Optional[List[str]] = None):
+        self.servers = {}
+        self.available_calls = {'ping' : self._process_ping,
+                                'status' : self._process_status}
 
-    async def on_message(self, message):
+        logger.info(f"Available commands: {list(self.available_calls.keys())}")
+
+        if disabled_calls is not None:
+            self.disabled_calls = disabled_calls
+            logger.info(f"Disabled commands: {self.disabled_calls}")
+
+        for s in servers:
+            server = {}
+            server['ip'] = s['ip']
+            server['port'] = s['port']
+            server['auth'] = aiohttp.BasicAuth(login=s['username'],
+                                               password=s['password'],
+                                               encoding='utf-8')
+            self.servers[s['name']] = server
+        
+        super().__init__(intents=intents)
+
+    def _on_ready(self):
+        print(f'We have logged in as {self.user}. Tracking {len(servers)} freqtrade servers')
+
+    async def process_command(self, 
+                              server: str,
+                              command: str,
+                              command_args: Optional[List[Any]]) -> Dict:
+        ip = servers[server]['ip']
+        port = servers[server]['port']
+        auth = servers[server]['auth']
+        base_url = f"http://{ip}:{port}/api/v1"
+
+        cmd = command.replace("$","")
+
+        if cmd in self.available_calls and cmd not in self.disabled_calls:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f'{base_url}/{cmd}') as r:
+                    if r.status == 200:
+                        js = await r.json()
+                        return self.available_calls[cmd](js, command_args)
+                    else:
+                        raise Exception(f"Error: Status {r.status} received.")
+        else:
+            raise Exception(f"Function '{cmd}' not available or is disabled by the server admin.")
+        
+        return None
+
+    def _process_ping(self, data: Dict, command_args = None):
+        embeds = [
+            {"title":"PING",
+            "fields":[{"name":"Response","value":data['status']}]
+            }
+        ]
+        embed = discord.Embed.from_dict(embeds[0])
+        return embed
+
+    def _process_status(self, data: Dict, command_args = None):
+        msg = []
+        headers = ["ID","PAIR","PROFIT %","PROFIT"]
+        msg.append(headers)
+        for trade in js:
+            msg.append(
+                [trade['trade_id'],
+                trade['pair'],
+                trade['current_profit_pct'],
+                f"{trade['current_profit_abs']} {trade['quote_currency']}"
+                ]
+            )
+        table = tabulate(msg, headers='firstrow', tablefmt='grid')
+        embed = f"```{table}```"
+        return embed
+
+    def send_msg(self, server: str, msg: Dict, title: str) -> Dict:
+        msg['bot'] = server
+        color = 0x0000FF
+        if msg['type'] in (RPCMessageType.EXIT, RPCMessageType.EXIT_FILL):
+            profit_ratio = msg.get('profit_ratio')
+            color = (0x00FF00 if profit_ratio > 0 else 0xFF0000)
+        if 'pair' in msg:
+            title = f"Trade: {msg['pair']} {msg['type'].value}"
+        embeds = [{
+            'title': title,
+            'color': color,
+            'fields': [],
+        }]
+        for f in fields:
+            for k, v in f.items():
+                v = v.format(**msg)
+                embeds[0]['fields'].append(
+                    {'name': k, 'value': v, 'inline': True})
+
+        # Send the message to discord channel
+        payload = {'embeds': embeds}
+        return payload
+
+    async def on_message(self, message: str) -> None:
         # don't let the bot reply to itself
         if message.author == self.user:
             return
@@ -60,51 +154,24 @@ class ft_bot(discord.Client):
             table = tabulate(resp,headers='firstrow',tablefmt='grid')
             await message.channel.send(f"```{table}```")
         else:
-            if len(cmd_string) > 1:
-                server = cmd_string[1]
+            cmd_args = []
+
+            if len(servers) == 1:
+                server = servers.keys()[0]
+                cmd_args = cmd_string[1:]
             else:
-                return "ERROR: No server specified. Run `$servers` to list them."
+                if len(cmd_string) > 1:
+                    server = cmd_string[1]
+                    cmd_args = cmd_string[2:]
+                else:
+                    await message.channel.send("ERROR: No server specified. Run `$servers` to list them.")
 
-            ip = servers[server]['ip']
-            port = servers[server]['port']
-            auth = servers[server]['auth']
-            base_url = f"http://{ip}:{port}/api/v1"
-
-            if cmd.startswith('$ping'):
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(f'{base_url}/ping') as r:
-                        if r.status == 200:
-                            js = await r.json()
-                            embeds = [
-                                {"title":"PING",
-                                "fields":[{"name":"Response","value":js['status']}]
-                                }
-                            ]
-                            embed = discord.Embed.from_dict(embeds[0])
-                            await message.channel.send(embed=embed)
-                        else:
-                            await message.channel.send(f"Error: Status {r.status} received.")
-
-            if cmd.startswith('$status'):
-                async with aiohttp.ClientSession(auth=auth) as session:
-                    async with session.get(f'{base_url}/status') as r:
-                        if r.status == 200:
-                            js = await r.json()
-                            resp = []
-                            headers = ["ID","PAIR","PROFIT %","PROFIT"]
-                            resp.append(headers)
-                            for trade in js:
-                                resp.append(
-                                    [trade['trade_id'],
-                                    trade['pair'],
-                                    trade['current_profit_pct'],
-                                    f"{trade['current_profit_abs']} {trade['quote_currency']}"
-                                    ]
-                                )
-                            table = tabulate(resp,headers='firstrow',tablefmt='grid')
-                            await message.channel.send(f"```{table}```")
-                        else:
-                            await message.channel.send(f"Error: Status {r.status} received.")
+                try:
+                    embed = self.process_command(server, cmd, cmd_args)
+                    if embed is not None:
+                        await message.channel.send(embed=embed)
+                except Exception as e:
+                    await message.channel.send(f"ERROR: {e}")
 
 
 class dotdict(dict):
@@ -129,21 +196,27 @@ def main(args):
             args.yaml = True
 
     if args.yaml:
-        token = args.token
-        for s in args.servers:
-            server = {}
-            server['ip'] = s['ip']
-            server['port'] = s['port']
-            server['auth'] = aiohttp.BasicAuth(login=s['username'],
-                                               password=s['password'],
-                                               encoding='utf-8')
-            servers[s['name']] = server
+        if not args.token or args.token == "":
+            raise Exception("No discord token supplied.")
+
+        if len(args.servers) == 0:
+            raise Exception("No freqtrade servers supplied.")
 
         intents = discord.Intents.default()
         intents.message_content = True
-        client = ft_bot(intents=intents)
 
-        client.run(token)
+        try:
+            if args.disabled_calls and len(args.disabled_calls) > 0:
+                client = ft_bot(intents=intents,
+                                servers=args.servers,
+                                disabled_calls=args.disabled_calls)
+            else:
+                client = ft_bot(intents=intents,
+                                servers=args.servers)
+                                
+            client.run(args.token)
+        except Exception as e:
+            logger.error(f"Cannot start ft_bot: ", e)
     else:
         raise Exception("No YAML file supplied.")
 
@@ -155,4 +228,4 @@ if __name__ == "__main__":
 
     except Exception as e:
         traceback.print_exc()
-        print("You got frogged: ", e)
+        logger.error("You got frogged: ", e)
